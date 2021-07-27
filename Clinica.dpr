f56224fd572280e008485ead220e085a193eedf8
Sabrina <sabrinaschmidt335@gmail.com>
2021-07-26 21:25:07 -0300

    Finalização de itens de banco e novos itens de interface

--- Clinica.dpr
+++ Clinica.dpr
@@ -2,13 +2,17 @@ program Clinica;
 
 uses
   Vcl.Forms,
-  UnitMain in 'UnitMain.pas' {Form1};
+  UnitMain in 'UnitMain.pas' {FormMain},
+  UnitCad in 'UnitCad.pas' {FormCad},
+  UnitCadAgendamento in 'UnitCadAgendamento.pas' {FormCadAgendamento};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TFormCad, FormCad);
+  Application.CreateForm(TFormCadAgendamento, FormCadAgendamento);
   Application.Run;
 end.
